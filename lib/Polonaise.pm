ea38ef5d45b74a35d60722c7b021e8c35723d358
ambs <ambs@20beef0f-d32d-4668-87ae-5289c4fabac0>
2012-02-08 14:39:29 +0000

    upload !?
    git-svn-id: http://svn.perl-hackers.net/polonaise@3 20beef0f-d32d-4668-87ae-5289c4fabac0

--- lib/Polonaise.pm
+++ lib/Polonaise.pm
@@ -1,10 +1,20 @@
 package Polonaise;
 use Dancer ':syntax';
 
+use File::Path 'make_path';
+use File::Spec;
+
 our $VERSION = '0.1';
 
 get '/' => sub {
     template 'index';
+};
+
+post '/upload/**' => sub {
+    ## this will use the filename POST'ed
+    my ($path) = splat;
+    my $file = upload('filename');
+    $file->copy_to(File::Spec->catfile(setting('public'), @$path, $file->basename));
 };
 
 true;
